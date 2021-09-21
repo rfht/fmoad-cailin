9f296d728ea31d17a755e25c657c1273a199c32d
thfrwn <11335318+rfht@users.noreply.github.com>
2021-09-20 20:29:41 -0600

    disable struct StreamingAudioData for now; fix some minor syntax errors

--- fmodstudio.c
+++ fmodstudio.c
@@ -1,46 +1,47 @@
 #include "fmodstudio.h"
+#include "al.h"
 
-int FMOD_Studio_System_Create()
+FMOD_RESULT FMOD_Studio_System_Create()
 {
 	STUB();
 }
 
-int FMOD_Studio_System_Initialize()
+FMOD_RESULT FMOD_Studio_System_Initialize()
 {
 	STUB();
 }
 
-int FMOD_Studio_System_SetListenerAttributes()
+FMOD_RESULT FMOD_Studio_System_SetListenerAttributes()
 {
 	STUB();
 }
 
-int FMOD_Studio_System_Update(int *system)
+FMOD_RESULT FMOD_Studio_System_Update(int *system)
 {
 	STUB();
 }
 
-int FMOD_Studio_System_LoadBankFile(int *system, char *filename, int *flags, int **bank)
+FMOD_RESULT FMOD_Studio_System_LoadBankFile(int *system, char *filename, int *flags, int **bank)
 {
 	STUB();
 }
 
-int FMOD_Studio_System_GetVCA(int *system, char *path, int **vca)
+FMOD_RESULT FMOD_Studio_System_GetVCA(int *system, char *path, int **vca)
 {
 	STUB();
 }
 
-int FMOD_Studio_VCA_SetVolume(int *vca, float volume)
+FMOD_RESULT FMOD_Studio_VCA_SetVolume(int *vca, float volume)
 {
 	STUB();
 }
 
-int FMOD_Studio_VCA_GetVolume(int *vca, float *volume, float *finalvolume)
+FMOD_RESULT FMOD_Studio_VCA_GetVolume(int *vca, float *volume, float *finalvolume)
 {
 	STUB();
 }
 
-int FMOD_Studio_System_GetEvent(int *system, char *path, int **event)
+FMOD_RESULT FMOD_Studio_System_GetEvent(int *system, char *path, int **event)
 {
 	FILE *f;
 	f = fopen("fmod.log", "a");
@@ -54,23 +55,23 @@ int FMOD_Studio_System_GetEvent(int *system, char *path, int **event)
 	return 0;
 }
 
-int FMOD_Studio_EventDescription_LoadSampleData(int *eventdescription)
+FMOD_RESULT FMOD_Studio_EventDescription_LoadSampleData(int *eventdescription)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventDescription_CreateInstance(int *eventdescription, int **instance)
+FMOD_RESULT FMOD_Studio_EventDescription_CreateInstance(int *eventdescription, int **instance)
 {
 	STUB();
 }
 
 
-int FMOD_Studio_EventDescription_Is3D(int *eventdescription, int *is3D)
+FMOD_RESULT FMOD_Studio_EventDescription_Is3D(int *eventdescription, int *is3D)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_Start(int *eventinstance)
+FMOD_RESULT FMOD_Studio_EventInstance_Start(int *eventinstance)
 {
 	FILE *f;
 	f = fopen("fmod.log", "a");
@@ -79,22 +80,22 @@ int FMOD_Studio_EventInstance_Start(int *eventinstance)
 	return 0;
 }
 
-int FMOD_Studio_System_GetBus(int *system, char *path, int **bus)
+FMOD_RESULT FMOD_Studio_System_GetBus(int *system, char *path, int **bus)
 {
 	STUB();
 }
 
-int FMOD_Studio_Bus_SetPaused(int *bus, int paused)
+FMOD_RESULT FMOD_Studio_Bus_SetPaused(int *bus, int paused)
 {
 	STUB();
 }
 
-int FMOD_Studio_Bus_GetPaused(int *bus, int *paused)
+FMOD_RESULT FMOD_Studio_Bus_GetPaused(int *bus, int *paused)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_GetDescription(int *eventinstance, int **description)
+FMOD_RESULT FMOD_Studio_EventInstance_GetDescription(int *eventinstance, int **description)
 {
 	FILE *f;
 	f = fopen("fmod.log", "a");
@@ -102,52 +103,52 @@ int FMOD_Studio_EventInstance_GetDescription(int *eventinstance, int **descripti
 	return 0;
 }
 
-int FMOD_Studio_EventDescription_GetPath(int *eventdescription, char *path, int size, int *retrieved)
+FMOD_RESULT FMOD_Studio_EventDescription_GetPath(int *eventdescription, char *path, int size, int *retrieved)
 {
 	FILE *f;
 	f = fopen("fmod.log", "a");
 	fprintf(f, "FMOD_Studio_EventDescription_GetPath: %s, %d\n", path, size);
-	path = "event:/env/amb/worldmap\0";	# corresponds to ./Content/FMOD/Desktop/sfx.banko/sfx-env_amb_worldmap.ogg
+	path = "event:/env/amb/worldmap\0";	// corresponds to ./Content/FMOD/Desktop/sfx.banko/sfx-env_amb_worldmap.ogg
 	*retrieved = 24;
 	return 0;
 }
 
-int FMOD_Studio_Bank_LoadSampleData(int *bank)
+FMOD_RESULT FMOD_Studio_Bank_LoadSampleData(int *bank)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_SetVolume(int *eventinstance, float volume)
+FMOD_RESULT FMOD_Studio_EventInstance_SetVolume(int *eventinstance, float volume)
 {
 	STUB();
 }
 
-int FMOD_Studio_System_GetLowLevelSystem(int *system, int **lowLevelSystem)
+FMOD_RESULT FMOD_Studio_System_GetLowLevelSystem(int *system, int **lowLevelSystem)
 {
 	STUB();
 }
 
-int FMOD_Studio_System_GetListenerAttributes(int *system, int listener, int *attributes)
+FMOD_RESULT FMOD_Studio_System_GetListenerAttributes(int *system, int listener, int *attributes)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_Set3DAttributes(int *eventinstance, int *attributes)
+FMOD_RESULT FMOD_Studio_EventInstance_Set3DAttributes(int *eventinstance, int *attributes)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_Release(int *eventinstance)
+FMOD_RESULT FMOD_Studio_EventInstance_Release(int *eventinstance)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_GetVolume(int *eventinstance, float *volume, float *finalvolume)
+FMOD_RESULT FMOD_Studio_EventInstance_GetVolume(int *eventinstance, float *volume, float *finalvolume)
 {
-	STUB()
+	STUB();
 }
 
-int FMOD_Studio_EventInstance_Stop(int *eventinstance, int mode)
+FMOD_RESULT FMOD_Studio_EventInstance_Stop(int *eventinstance, int mode)
 {
 	FILE *f;
 	f = fopen("fmod.log", "a");
@@ -155,47 +156,47 @@ int FMOD_Studio_EventInstance_Stop(int *eventinstance, int mode)
 	return 0;
 }
 
-int FMOD_Studio_EventInstance_Get3DAttributes(int *eventinstance, int *attributes)
+FMOD_RESULT FMOD_Studio_EventInstance_Get3DAttributes(int *eventinstance, int *attributes)
 {
 	STUB();
 }
 
-int FMOD_Studio_System_Release(int *system)
+FMOD_RESULT FMOD_Studio_System_Release(int *system)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_SetParameterValue(int *eventinstance, char *name, float value)
+FMOD_RESULT FMOD_Studio_EventInstance_SetParameterValue(int *eventinstance, char *name, float value)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventDescription_IsOneshot(int *eventdescription, int *oneshot)
+FMOD_RESULT FMOD_Studio_EventDescription_IsOneshot(int *eventdescription, int *oneshot)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_SetPaused(int *eventinstance, int paused)
+FMOD_RESULT FMOD_Studio_EventInstance_SetPaused(int *eventinstance, int paused)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_TriggerCue(int *eventinstance)
+FMOD_RESULT FMOD_Studio_EventInstance_TriggerCue(int *eventinstance)
 {
 	STUB();
 }
 
-int FMOD_Studio_Bus_StopAllEvents(int *bus, int mode)
+FMOD_RESULT FMOD_Studio_Bus_StopAllEvents(int *bus, int mode)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_GetPaused(int *eventinstance, int *paused)
+FMOD_RESULT FMOD_Studio_EventInstance_GetPaused(int *eventinstance, int *paused)
 {
 	STUB();
 }
 
-int FMOD_Studio_EventInstance_GetPlaybackState(int *eventinstance, int *state)
+FMOD_RESULT FMOD_Studio_EventInstance_GetPlaybackState(int *eventinstance, int *state)
 {
 	STUB();
 }
